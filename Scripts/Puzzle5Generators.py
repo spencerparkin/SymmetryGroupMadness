ef0c575558f77ebfc2abcd92bf2c779c6b3d03d2
Spencer Parkin <stparkin@mmm.com>
2017-04-21 12:23:44 -0600

    script to remove error in coming up with generators for the desired group

---
+++ Scripts/Puzzle5Generators.py
@@ -0,0 +1,73 @@
+# Puzzle5Generators.py
+
+matrix = []
+rows = 8
+cols = 10
+
+for i in range( rows ):
+    matrix_col = []
+    for j in range( cols ):
+        matrix_col.append({})
+    matrix.append( matrix_col )
+    
+for i in range( rows ):
+    for j in range( cols ):
+        entry = matrix[i][j]
+        entry[ 'used' ] = True
+        if i < 2 and j < 4:
+            entry[ 'used' ] = False
+        elif i >= 6 and j >= 6:
+            entry[ 'used' ] = False
+            
+number = 0
+for i in range( rows ):
+    for j in range( cols ):
+        entry = matrix[i][j]
+        if entry[ 'used' ]:
+            entry[ 'number' ] = number
+            number += 1
+
+def PrintGenerator( row, col, label, transform ):
+    print( '' )
+    print( 'Permutation %s;' % label )
+    print( '%s.DefineIdentity();' % label )
+    for i in range(6):
+        for j in range(6):
+            entry = matrix[ row + i ][ col + j ]
+            number = entry[ 'number' ]
+            x, y = transform( i, j )
+            other_entry = matrix[ row + x ][ col + y ]
+            other_number = other_entry[ 'number' ]
+            if number != other_number:
+                print( '%s.Define( %d, %d );' % ( label, number, other_number ) )
+
+def ReflectDiagonal( i, j ):
+    return j, i
+
+def ReflectOtherDiagonal( i, j ):
+    return 5 - i, 5 - j
+
+def ReflectVertical( i, j ):
+    return i, 5 - j
+
+def ReflectHorizontal( i, j ):
+    return 5 - i, j
+
+def RotateCCW( i, j ):
+    i, j = ReflectDiagonal( i, j )
+    i, j = ReflectHorizontal( i, j )
+    return i, j
+    
+PrintGenerator( 0, 4, 'permR0', RotateCCW )
+PrintGenerator( 0, 4, 'permD0p', ReflectOtherDiagonal )
+PrintGenerator( 0, 4, 'permV0', ReflectVertical )
+PrintGenerator( 0, 4, 'PermD0', ReflectDiagonal )
+PrintGenerator( 0, 4, 'PermH0', ReflectHorizontal )
+
+PrintGenerator( 2, 0, 'permR1', RotateCCW )
+PrintGenerator( 2, 0, 'permD1p', ReflectOtherDiagonal )
+PrintGenerator( 2, 0, 'permV1', ReflectVertical )
+PrintGenerator( 2, 0, 'PermD1', ReflectDiagonal )
+PrintGenerator( 2, 0, 'PermH1', ReflectHorizontal )
+                
+# Puzzle5Generators.py
